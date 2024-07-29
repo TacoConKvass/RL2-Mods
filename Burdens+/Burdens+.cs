ea6f22376a0d8bae7473cb4143bb8ab74bf7c169
TacoConKvass <e_frun@o2.pl>
2024-07-29 19:46:14 +0200

    [Addition] - Burdens+ version 1.0.0

---
+++ Burdens+/Burdens+.cs
@@ -0,0 +1,25 @@
+﻿using MonoMod.RuntimeDetour;
+using RL2.ModLoader;
+using System;
+using System.Reflection;
+
+namespace BurdensPlus;
+
+[ModEntrypoint]
+public class BurdensPlus
+{
+	public BurdensPlus() {
+		ModLoader.OnLoad += GetBurdenDataHook.Apply;
+		ModLoader.OnUnload += GetBurdenDataHook.Undo;
+	}
+
+	public Hook GetBurdenDataHook = new Hook(
+		typeof(BurdenLibrary).GetMethod("GetBurdenData", BindingFlags.Public | BindingFlags.Static),
+		(Func<BurdenType, BurdenData> orig, BurdenType type) => {
+			BurdenData burdenData = orig(type);
+			burdenData.MaxBurdenLevel = 99;
+			burdenData.Disabled = false;
+			return burdenData;
+		}
+    );
+}

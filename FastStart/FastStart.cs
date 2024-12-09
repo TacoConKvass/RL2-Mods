a03ee29605ceb8de9a648086ba8eeecf932e257a
TacoConKvass <e_frun@o2.pl>
2024-12-09 15:19:30 +0100

    [Addition] - Fast Start

---
+++ FastStart/FastStart.cs
@@ -0,0 +1,30 @@
+﻿using RL2.API;
+using System;
+
+namespace FastStart;
+
+public class FastStart : Mod {
+	public override void OnLoad() {
+		Player.OnSpawn += GrantBonuses;
+	}
+	public override void OnUnload() {
+		Player.OnSpawn -= GrantBonuses;
+	}
+
+	public static void GrantBonuses(PlayerController player) {
+		foreach (HeirloomType val in new HeirloomType[] {
+			HeirloomType.UnlockVoidDash,
+			HeirloomType.UnlockAirDash,
+			HeirloomType.UnlockDoubleJump,
+			HeirloomType.UnlockBouncableDownstrike,
+			HeirloomType.CaveLantern,
+			HeirloomType.UnlockMemory,
+			HeirloomType.UnlockEarthShift,
+		}) {
+			SaveManager.PlayerSaveData.SetHeirloomLevel(val, 1, false, false);
+		}
+		SaveManager.PlayerSaveData.SetInsightState(InsightType.ForestBoss_DoorOpened, InsightState.ResolvedButNotViewed, true);
+		SaveManager.PlayerSaveData.SetFlag(PlayerSaveFlag.CaveMiniboss_BlackDoor_Opened, true);
+		SaveManager.PlayerSaveData.SetFlag(PlayerSaveFlag.CaveMiniboss_WhiteDoor_Opened, true);
+	}
+}

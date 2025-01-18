d7c4bb62dfe05b1f5edf9b953e1746792cecd88c
TacoConKvass <e_frun@o2.pl>
2025-01-18 21:00:29 +0100

    [Addition] Ladies Only

---
+++ LadiesOnly/LadiesOnly.cs
@@ -0,0 +1,24 @@
+﻿using MonoMod.RuntimeDetour;
+using RL2.ModLoader;
+using System;
+using System.Reflection;
+
+namespace LadiesOnly;
+
+[ModEntrypoint]
+public class LadiesOnly
+{
+	public LadiesOnly() {
+		ModLoader.OnLoad += HeirIsAlwaysFemale_Hook.Apply;
+		ModLoader.OnUnload += HeirIsAlwaysFemale_Hook.Undo;
+    }
+
+	public Hook HeirIsAlwaysFemale_Hook = new Hook(
+		typeof(CharacterCreator).GetMethod("GetRandomGender", BindingFlags.Public | BindingFlags.Static),
+		(Func<bool, bool> orig, bool useUnityRandom) => true,
+		new HookConfig() {
+			ID = "LadiesOnly::HeirIsAlwaysFemale",
+			ManualApply = true
+		}
+	);
+}
